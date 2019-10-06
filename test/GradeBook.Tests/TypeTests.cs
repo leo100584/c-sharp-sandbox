0373d51618df647196a815ca60560a7033edd69d
Lars Erik Opdal <Lars.Erik.Opdal@triona.no>
2019-10-06 11:38:43 +0200

    Adding a few more tests for reference and value behavior

--- test/GradeBook.Tests/TypeTests.cs
+++ test/GradeBook.Tests/TypeTests.cs
@@ -4,7 +4,76 @@ using Xunit;
 namespace GradeBook.Tests
 {    
     public class TypeTests
-    {
+    {   [Fact]
+        public void StringsbehaveLikeValutypes(){
+            string name = "Leo";
+            var upper = MakeUppercase(name);
+
+            Assert.Equal("Leo",name);
+            Assert.Equal("LEO",upper);
+
+        }
+
+        private string MakeUppercase(string parameter)
+        {
+            return parameter.ToUpper();
+        }
+
+        [Fact]
+        public void ValuTypesAlsoPassByValue(){
+            var x = GetInt();
+            SetInt(ref x);
+
+            Assert.Equal(42,x);
+        }
+        private void SetInt(ref int z)
+        {
+            z = 42;
+        }
+        private int GetInt()
+        {
+            return 3;
+        }
+
+        [Fact]
+        public void CSharpIsPassByRef(){
+             var book1 = GetBook("Book 1");
+             GetBookSetName(ref book1,"New Name");
+            Assert.NotEqual("new Name",book1.Name);
+        
+        }
+
+        private void GetBookSetName(ref Book book, string name)
+        {
+            book = new Book(name);
+        }
+
+        [Fact]
+        public void CSharpIsPassByValue(){
+             var book1 = GetBook("Book 1");
+             GetBookSetName(book1,"New Name");
+            Assert.NotEqual("new Name",book1.Name);
+        
+        }
+
+        private void GetBookSetName(Book book, string name)
+        {
+            book = new Book(name);
+        }
+
+        [Fact]
+        public void CanSetNameFromReference(){
+             var book1 = GetBook("Book 1");
+             SetName(book1,"New Name");
+            Assert.NotEqual("new Name",book1.Name);
+        
+        }
+        
+        private void SetName(Book book, string name)
+        {
+            book.Name = name;
+        }
+
         [Fact]
         public void GetBookReturnsDifferentObjects()
         {
